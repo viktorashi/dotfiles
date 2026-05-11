a1638d6cf3ea39f702650d312ca2bb659efcb3e1
viktorashi <ioanvictorstan@gmail.com>
2026-05-11 21:40:04 +0300

    more general terminfo

--- docs/legacy-shi/copy-terminfo.sh
+++ docs/legacy-shi/copy-terminfo.sh
@@ -1,13 +1,15 @@
 # if whatever os you're ssh'ed into doesn't support the terminal your currently using (you cant clear and other things)
 
 #if the host os supports it
-infocmp -x ghostty | ssh centos-vm "tic -x -"
+remote_name="acas"
+infocmp -x ghostty | ssh $remote_name "tic -x -"
 
 #WARNING: Some older tic's dont support reading from stdin, so you gotta do this next two step proccess detailed:
 #from local (newer) machine:
 infocmp -x ghostty >ghostty.terminfo
-scp ghostty.terminfo centos-vm:~/
+scp ghostty.terminfo $remote_name:~/
+rm ghostty.terminfo
 
 #from older:
-# then after compiling it from inside the VM
+#then from inside the remote
 tic -x ghostty.terminfo
