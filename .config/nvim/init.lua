b6091ce0bcff7bf766023bc7da7493365502744e
istan <i.stan@stratec.com>
2026-05-30 16:04:20 +0300

    but now we go back, cuz it don't really work
    This reverts commit 062e4afe180042fc46f8ab1862d47fbf2c98ad62

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.api.nvim_create_user_command(
-  "StartupList",
-  function()
-    local output = vim.fn.system(
-      'powershell.exe -File "C:\\Users\\istan\\docs\\vindovs\\manage-startup-apps.ps1" -Action list'
-    )
-    print(output)
-  end,
-  {}
-)
