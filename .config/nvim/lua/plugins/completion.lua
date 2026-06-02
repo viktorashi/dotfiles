eb4f563cc1a4cc7d41a1d40833796b18533f52f5
viktorashi <ioanvictorstan@gmail.com>
2026-06-02 13:30:46 +0300

    fixed LSP bugs

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,25 +1,27 @@
 return {
-  -- Disable buffer completion source for blink.cmp (default in newer LazyVim versions)
+  -- Disable buffer and snippets completion sources for blink.cmp (default in newer LazyVim versions)
   {
     "saghen/blink.cmp",
-    opts = function(_, opts)
-      if opts.sources and opts.sources.default then
-        opts.sources.default = vim.tbl_filter(function(source)
-          return source ~= "buffer"
-        end, opts.sources.default)
-      end
-    end,
+    opts = {
+      sources = {
+        providers = {
+          buffer = { enabled = false },
+          snippets = { enabled = false },
+        },
+      },
+    },
   },
 
-  -- Disable buffer completion source for nvim-cmp (if active instead)
+  -- Disable buffer and snippets completion sources for nvim-cmp (if active instead)
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       if opts.sources then
         opts.sources = vim.tbl_filter(function(source)
-          return source.name ~= "buffer"
+          return source.name ~= "buffer" and source.name ~= "snippets"
         end, opts.sources)
       end
+      return opts
     end,
   },
 }
