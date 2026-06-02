28d857cc45a42db88ecc6d9f51dfbed4602ed8e3
viktorashi <ioanvictorstan@gmail.com>
2026-06-02 18:37:56 +0300

    completion

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,30 @@
+return {
+  -- Configure blink.cmp (LazyVim's default completion engine)
+  {
+    "saghen/blink.cmp",
+    opts = {
+      fuzzy = {
+        sorts = {
+          -- Deprioritize python dunder methods (__)
+          function(a, b)
+            local a_is_dunder = a.label:sub(1, 2) == "__"
+            local b_is_dunder = b.label:sub(1, 2) == "__"
+            if a_is_dunder ~= b_is_dunder then
+              return b_is_dunder
+            end
+            return nil
+          end,
+          -- Fallback to default sorts
+          "score",
+          "sort_text",
+        },
+      },
+      sources = {
+        providers = {
+          buffer = { enabled = false },
+          snippets = { enabled = false },
+        },
+      },
+    },
+  },
+}
