30176f83328df001d903438bdf3642ec24e5ce16
viktorashi <viktorashi@viktorashis-MacBook-Air.local>
2026-06-06 20:33:42 +0300

    broot intsall with nice thingies

--- docs/install-init-stuff.sh
+++ docs/install-init-stuff.sh
@@ -1,5 +1,9 @@
-brew install opera-gx alt-tab ghostty broot bat bat-extras colima raycast fzf lazygit tmux neovim gh battery git-delta zoxide
+brew install opera-gx alt-tab ghostty bat bat-extras colima raycast fzf lazygit tmux neovim gh battery git-delta zoxide
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
 nvm install lts
 nvm use --lts
+
+curl https://sh.rustup.rs -sSf | sh
+
+( git clone https://github.com/Canop/broot ~/broot && cd ~/broot && cargo install --locked --features clipboard --path . && cd ../ && rm -rf ~/broot ) || brew install broot
