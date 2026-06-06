72c272a50ca368478528e32710e782e6f319aa52
viktorashi <viktorashi@viktorashis-MacBook-Air.local>
2026-06-06 20:59:53 +0300

    trackin cargo

--- docs/install-init-stuff.sh
+++ docs/install-init-stuff.sh
@@ -1,9 +1,22 @@
-brew install opera-gx alt-tab ghostty bat bat-extras colima raycast fzf lazygit tmux neovim gh battery git-delta zoxide
+brew install opera-gx alt-tab ghostty bat bat-extras colima raycast fzf lazygit tmux neovim gh battery git-delta zoxide cargo-binstall
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install lts
 nvm use --lts
 
 command -v rustup >/dev/null 2>&1 || curl https://sh.rustup.rs -sSf | sh -s -- -y
+[ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
+cargo binstall sccache
 
-( git clone https://github.com/Canop/broot ~/broot && cd ~/broot && cargo install --locked --features clipboard --path . && cd ../ && rm -rf ~/broot ) || brew install broot
+cat >> $HOME/.cargo/config.toml <<- EOM
+[build]
+rustc-wrapper = "$(which sccache)"
+EOM
+
+#install broot with nice compiler options
+if [ ! -d ~/broot ]; then
+  git clone https://github.com/Canop/broot ~/broot
+fi
+( cd ~/broot && cargo install --locked --features clipboard --path . ) || brew install broot
