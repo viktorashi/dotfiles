6d997745ff1ac9d0b288a9dd0cbf4ccaf38bc921
viktorashi <viktorashi@viktorashis-MacBook-Air.local>
2026-06-06 20:17:38 +0300

    script to install verything mac

---
+++ docs/install-init-stuff.sh
@@ -0,0 +1,5 @@
+brew install opera-gx alt-tab ghostty broot bat bat-extras colima raycast fzf lazygit tmux neovim gh battery git-delta zoxide
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
+nvm install lts
+nvm use --lts
