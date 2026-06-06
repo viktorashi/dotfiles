f9d955b7e4d68630b3dd2261b17c4a3def6ad307
viktorashi <viktorashi@viktorashis-MacBook-Air.local>
2026-06-06 20:34:54 +0300

    only if installed

--- docs/install-init-stuff.sh
+++ docs/install-init-stuff.sh
@@ -4,6 +4,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
 nvm install lts
 nvm use --lts
 
-curl https://sh.rustup.rs -sSf | sh
+command -v rustup >/dev/null 2>&1 || curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 ( git clone https://github.com/Canop/broot ~/broot && cd ~/broot && cargo install --locked --features clipboard --path . && cd ../ && rm -rf ~/broot ) || brew install broot
