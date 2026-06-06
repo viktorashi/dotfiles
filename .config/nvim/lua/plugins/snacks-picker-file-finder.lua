316547270e1352b269746dc626d61f20dd1520a8
viktorashi <ioanvictorstan@gmail.com>
2026-06-06 22:44:48 +0300

    sncaks nice

---
+++ .config/nvim/lua/plugins/snacks-picker-file-finder.lua
@@ -0,0 +1,27 @@
+-- A global setting in snacks.nvim's picker configuration does not work because
+-- individual picker sources often override or ignore top-level defaults,
+-- requiring specific configuration per source to ensure consistent behavior.
+
+local picker_sources =
+  { "files", "grep", "smart", "explorer" }
+local sources = {}
+for _, source in ipairs(picker_sources) do
+  sources[source] = {
+    hidden = true,
+    ignored = false,
+  }
+end
+
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        db = {
+          sqlite3_path = "/lib/x86_64-linux-gnu/libsqlite3.so.0",
+        },
+        sources = sources,
+      },
+    },
+  },
+}
