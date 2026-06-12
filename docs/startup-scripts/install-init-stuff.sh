9963d326f01dbcfe683b08ef2db4a7f884e54fe4
viktorashi <ioanvictorstan@gmail.com>
2026-06-12 15:00:59 +0300

    mai idemptotent

--- docs/startup-scripts/install-init-stuff.sh
+++ docs/startup-scripts/install-init-stuff.sh
@@ -5,7 +5,10 @@ bash ~/docs/startup-scripts/git-settings.sh
 source ~/docs/conf.sh
 mkdir -p "$HOME/.local/share/editor-packages" "$HOME/.local/share/nvim/site/pack/codex/start" "$HOME/.vim/pack/codex/start" "$HOME/.config/nvim/plugin" "$HOME/.vim/plugin"
 
-[ -d "$HOME/.local/share/editor-packages/vim-obsession/.git" ] || git clone --depth 1 https://github.com/tpope/vim-obsession.git "$HOME/.local/share/editor-packages/vim-obsession"
+if [ ! -d "$HOME/.local/share/editor-packages/vim-obsession/.git" ]; then
+  rm -rf "$HOME/.local/share/editor-packages/vim-obsession"
+  git clone --depth 1 https://github.com/tpope/vim-obsession.git "$HOME/.local/share/editor-packages/vim-obsession"
+fi
 ln -sfn "$HOME/.local/share/editor-packages/vim-obsession" "$HOME/.local/share/nvim/site/pack/codex/start/vim-obsession"
 ln -sfn "$HOME/.local/share/editor-packages/vim-obsession" "$HOME/.vim/pack/codex/start/vim-obsession"
 ln -sfn "$HOME/docs/startup-scripts/obsession-bootstrap.vim" "$HOME/.config/nvim/plugin/obsession-bootstrap.vim"
