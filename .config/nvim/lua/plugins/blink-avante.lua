a09f1d7bbecf3f1d1ef8fdb4917331309ac8f4e4
istan <i.stan@stratec.com>
2026-07-03 16:24:39 +0300

    mai scurticel si frumusel

--- .config/nvim/lua/plugins/blink-avante.lua
+++ .config/nvim/lua/plugins/blink-avante.lua
@@ -1,21 +1,13 @@
 return {
   "Saghen/blink.cmp",
   optional = true,
-  dependencies = {
-    "Kaiser-Yang/blink-cmp-avante",
+  dependencies = { "Kaiser-Yang/blink-cmp-avante" },
+  opts = {
+    sources = {
+      default = { "avante" },
+      providers = {
+        avante = { module = "blink-cmp-avante", name = "Avante" },
+      },
+    },
   },
-  opts = function(_, opts)
-    opts.sources = opts.sources or {}
-    opts.sources.default = opts.sources.default or {}
-    opts.sources.providers = opts.sources.providers or {}
-
-    if not vim.tbl_contains(opts.sources.default, "avante") then
-      table.insert(opts.sources.default, 1, "avante")
-    end
-
-    opts.sources.providers.avante = {
-      module = "blink-cmp-avante",
-      name = "Avante",
-    }
-  end,
 }
--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -4,11 +4,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
-
-      -- Only one of these is needed.
-      "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua", -- optional
-      "nvim-mini/mini.pick", -- optional
       "folke/snacks.nvim", -- optional
     },
     keys = {
