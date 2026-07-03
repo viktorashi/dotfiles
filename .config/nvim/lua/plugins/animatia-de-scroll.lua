8841bfa1a4f53ad639dc4a82ad8e87a7dd036ea8
istan <i.stan@stratec.com>
2026-07-03 16:31:44 +0300

    this used to be in keymaps b4

--- .config/nvim/lua/plugins/animatia-de-scroll.lua
+++ .config/nvim/lua/plugins/animatia-de-scroll.lua
@@ -7,5 +7,31 @@ return {
     },
   },
   --si foloseste-o pe asta mai buna
-  { "karb94/neoscroll.nvim", opts = {} },
+  {
+    "karb94/neoscroll.nvim",
+    opts = {
+      mappings = {
+        "<C-u>",
+        "<C-d>",
+        "<C-b>",
+        "<C-f>",
+        "<C-y>",
+        "<C-e>",
+        "zt",
+        "zz",
+        "zb",
+      },
+      hide_cursor = true,
+      stop_eof = true,
+      respect_scrolloff = false,
+      cursor_scrolls_alone = true,
+      duration_multiplier = 0.7,
+      easing = "linear",
+      performance_mode = false,
+      ignored_events = {
+        "WinScrolled",
+        "CursorMoved",
+      },
+    },
+  },
 }
