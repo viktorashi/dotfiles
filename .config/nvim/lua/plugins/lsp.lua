cdae0653fc5cd1dbcad46e74f9e8c81aae8ae32b
viktorashi <ioanvictorstan@gmail.com>
2026-07-23 21:39:32 +0300

    lua lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,21 +8,38 @@ return {
           root_dir = function(fname)
             local util = require("lspconfig.util")
             -- Do not start pyrefly if ty.toml is present
-            if util.root_pattern("ty.toml")(fname) then
+            if
+              util.root_pattern("ty.toml")(fname)
+            then
               return nil
             end
-            return util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", ".git")(fname)
+            return util.root_pattern(
+              "pyproject.toml",
+              "setup.py",
+              "setup.cfg",
+              "requirements.txt",
+              ".git"
+            )(fname)
           end,
         },
         ty = {
           root_dir = function(fname)
             local util = require("lspconfig.util")
-            return util.root_pattern("ty.toml")(fname)
+            return util.root_pattern("ty.toml")(
+              fname
+            )
           end,
         },
         ruff_lsp = {},
         pyright = { enabled = false },
         basedpyright = { enabled = false },
+        gopls = {
+          settings = {
+            gopls = {
+              buildFlags = { "-tags=e2e" },
+            },
+          },
+        },
       },
     },
   },
