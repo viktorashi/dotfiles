22231d5e8500e1bcf9428e2900aa116399f9ac49
istan <istan@stratec.com>
2026-08-10 14:23:50 +0300

    not that much stuff needed here

--- files/shell/machines/arch-wsl.sh
+++ files/shell/machines/arch-wsl.sh
@@ -1,11 +1,7 @@
 # arch-wsl / leanoox — WSL2 under Windows, work laptop.
 
 # Windows tools reachable from inside WSL
-export PATH="$PATH:/mnt/c/Users/istan/AppData/Local/Programs/Git/bin"
-export PATH="$PATH:/mnt/c/Users/istan/AppData/Local/Programs/Git/usr/bin"
-export PATH="$PATH:/mnt/c/Users/istan/scoop/shims"
 export PATH="$PATH:/mnt/c/Program Files/Integrity/ILMClient13/bin/"
-[ -d /snap/bin ] && export PATH="$PATH:/snap/bin"
 
 # midnight commander vim keymap, installed to /etc by the mc package
 export MC_KEYMAP=/etc/mc/mc.vim.keymap
@@ -13,7 +9,3 @@ export MC_KEYMAP=/etc/mc/mc.vim.keymap
 # work: azure container registry
 export ACR_NAME="stratecai"
 export SERVICE_PRINCIPAL_NAME="ca04454e-62bc-4787-a26e-62469f8b5187"
-
-sshraspi() {
-	/mnt/c/Users/istan/repos-projects/SHWARMA_soft/script_kiddies/ssh-raspi.sh
-}
--- files/shell/machines/arch-wsl.zsh
+++
@@ -1,3 +0,0 @@
-# arch-wsl / leanoox
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
