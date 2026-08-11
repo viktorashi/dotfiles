a54afe482b0ec67039f8af65577cd8cabe458fc2
istan <istan@stratec.com>
2026-08-11 11:46:17 +0300

    pacman install

--- files/shell/machines/arch-wsl.sh
+++ files/shell/machines/arch-wsl.sh
@@ -9,3 +9,8 @@ export MC_KEYMAP=/etc/mc/mc.vim.keymap
 # work: azure container registry
 export ACR_NAME="stratecai"
 export SERVICE_PRINCIPAL_NAME="ca04454e-62bc-4787-a26e-62469f8b5187"
+
+alias pacman-install='sudo pacman -S --noconfirm'
+alias pacmanS=pacman-install
+alias yay-install='yay -S --noconfirm'
+alias yayS=yay-install
