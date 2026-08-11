4b07452ed834fb391dc8dd937131e85d531e08ee
istan <istan@stratec.com>
2026-08-12 01:00:06 +0300

    get it outta here

--- files/shell/machines/arch-wsl.sh
+++ files/shell/machines/arch-wsl.sh
@@ -26,3 +26,5 @@ alias yay-update='yay -Syu'
 
 eval "$(/usr/bin/wsl2-ssh-agent)"
 
+# zic aplicatii din Linux ca trebuie sa dea call la scoop, scoatel drq
+export PATH=$(echo "$PATH" | sed -e 's|:/mnt/c/Users/istan/scoop/shims||' -e 's|/mnt/c/Users/istan/scoop/shims:||')
