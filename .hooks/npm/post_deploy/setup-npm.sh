82ab3876ac472b19d7bee2dc1a526bc8f163e333
istan <istan@stratec.com>
2026-08-17 10:59:38 +0300

    mutat in hooks

--- .hooks/npm/post_deploy/setup-npm.sh
+++ .hooks/npm/post_deploy/setup-npm.sh
@@ -7,12 +7,12 @@ npm config set registry https://artifactory.intern.stratec.com/artifactory/api/n
 
 # Set CA file based on OS
 if [ -f /etc/ssl/certs/ca-certificates.crt ]; then
-    # Linux (Ubuntu/Debian/Arch)
-    npm config set cafile "/etc/ssl/certs/ca-certificates.crt" --global
+  # Linux (Ubuntu/Debian/Arch)
+  npm config set cafile "/etc/ssl/certs/ca-certificates.crt" --global
 elif [ -n "$WINDIR" ] || [[ "$(uname -r)" == *"microsoft"* ]]; then
-    # Windows or WSL fallback (pointing to the raw file in the repo)
-    # Using relative path from home directory or an absolute Windows path
-    npm config set cafile "$HOME/.dotfiles/files/certs/STRATEC-Chain.pem" --global
+  # Windows or WSL fallback (pointing to the raw file in the repo)
+  # Using relative path from home directory or an absolute Windows path
+  npm config set cafile "$HOME/.dotfiles/files/certs/STRATEC-Chain.pem" --global
 fi
 
 # PNPM automatically reads ~/.npmrc and global npmrc, so it will inherit these settings!
