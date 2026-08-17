8074ca1f04c453737912afe2e4ce718368bf08ee
istan <istan@stratec.com>
2026-08-17 10:52:07 +0300

    fix: resolve correct home directory in trust script when run via sudo

--- .hooks/certs/post_deploy/trust.sh
+++ .hooks/certs/post_deploy/trust.sh
@@ -4,7 +4,13 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-CERTS_DIR="$HOME/.dotfiles/files/certs"
+if [ -n "$SUDO_USER" ]; then
+    USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+else
+    USER_HOME=$HOME
+fi
+
+CERTS_DIR="$USER_HOME/.dotfiles/files/certs"
 echo "Installing custom certificates from $CERTS_DIR..."
 
 # Check if there are any certs to install
