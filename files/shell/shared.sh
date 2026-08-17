f4dda0eb7b4acc5e34ca3bd6b3ac91f92cb56e9f
istan <istan@stratec.com>
2026-08-17 12:04:28 +0300

    steam locomotive sa scap

--- files/shell/shared.sh
+++ files/shell/shared.sh
@@ -40,6 +40,7 @@ alias cl='clear'
 alias ..='cd ..'
 alias ...='cd ../../'
 
+alias sl='sl -e'
 alias ls='eza -a --classify --color=always --icons=always'
 alias l=ls
 alias ll="ls -l --sort=size --reverse --git --git-repos --total-size"
